a2bf4cb8528d63dcc2f1d51eb94da2bbcbda89b3
Mert Genç <mertgenc8@gmail.com>
2022-03-06 17:54:56 +0300

    add fish and terminator config.

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+alias ls "exa --long"
+alias glog "git log --all --decoreate --oneline --graph"
+
+
+alias dpsa "docker ps -a"
+alias dim  "docker images"
+alias cl "clear"
+alias editor "nvim"
+alias vim "nvim"
+alias s "sudo"
+alias conv "nvim ./config/nvim/init.vim"
+
